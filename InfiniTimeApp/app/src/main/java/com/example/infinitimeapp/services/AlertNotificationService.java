85cbe0d627e5f0791938aebcd74e0e6ce6623c9a
Robin Karlsson <robin.karlsson@protonmail.com>
2020-10-18 01:31:13 +0200

    implemented alert

--- InfiniTimeApp/app/src/main/java/com/example/infinitimeapp/services/AlertNotificationService.java
+++ InfiniTimeApp/app/src/main/java/com/example/infinitimeapp/services/AlertNotificationService.java
@@ -1,17 +1,17 @@
 package com.example.infinitimeapp.services;
 
+import com.example.infinitimeapp.bluetooth.BluetoothService;
+
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class AlertNotificationService extends BaseService {
-    private static final String NEW_ALERT = "NEW_ALERT";
+    public static final String NEW_ALERT = "NEW_ALERT";
 
     public AlertNotificationService() {
-        CHAR_MAP = Stream.of(new String[][]{
-                {NEW_ALERT, "00002a46-0000-1000-8000-00805f9b34fb"}
-        }).collect(Collectors.toMap(p -> p[0], p -> p[1]));
+        CHAR_MAP.put(NEW_ALERT, "00002a46-0000-1000-8000-00805f9b34fb");
     }
+
     @Override
     public void onDataRecieved(UUID characteristicName, byte[] message) {
         switch(getCharacteristicName(characteristicName)) {
@@ -19,5 +19,9 @@ public class AlertNotificationService extends BaseService {
                 break;
             default:
         }
+    }
+
+    public void sendMessage(String message) {
+        BluetoothService.getInstance().write(getCharacteristicUUID(NEW_ALERT), message.getBytes(StandardCharsets.US_ASCII));
     }
 }
